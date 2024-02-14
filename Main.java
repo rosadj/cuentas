6cb8da3904f2fb73f9a4dc3630004000f5b4c667
rosa <rosajuliakdk@hotmail.com>
2024-02-14 21:26:21 +0100

    proyecto entorns

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+        CCuenta cuenta1 =new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        double saldoActual;
+        operativa_cuenta(cuenta1);
+
+
+        public static void operativa_cuenta(CCuenta cuenta, float cantidad){
+            saldoActual = cuenta1.estado();
+            System.out.println("El saldo actual es"+ saldoActual );
+        }
+
+
+
+        try {
+            cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
